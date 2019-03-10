4a9706fa62d28c75b854311aaaeb4a4122f939bc
kupharmd05 <kupharmd05@gmail.com>
2019-03-10 16:55:55 -0500

    medications.js and server.js

--- models/medication.js
+++ models/medication.js
@@ -1,4 +1,4 @@
-module.exports = function (sequelize, DataTypes) {
+module.exports = function(sequelize, DataTypes) {
   var Medication = sequelize.define("Medication", {
     Generic_Name: {
       type: DataTypes.STRING,
