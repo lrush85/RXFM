9ebbf71e8ca309bd1b41c61b68a566bfd1e0eddc
Jaybird <jrmiller022@gmail.com>
2019-03-11 21:16:38 -0500

    renamed and made admin-database

--- models/seeds.sql
+++ models/seeds.sql
@@ -1,28 +1,56 @@
-insert into medications (Generic_Name, Brand_Name, Class, Uses, Side_Effects, Rationale, DC_Plan, Withdrawal)
-values ("Donepezil", "Aricept", "Acetylcholinesterase inhibitor", "Alzheimer's Dementia", "Nausea, Vomiting, Diarrhea, Insomnia","Medication is not proven to be effective at this stage of illness. Reduce pill burden and side effects.", "Tapering not required", "Delirium, Hallucinations, Dry Mouth, Constipation, Blurred Vision"); 
-insert into medications (Generic_Name, Brand_Name, Class, Uses, Side_Effects, Rationale, DC_Plan, Withdrawal)
-values ("Rivastigmine", "Exelon", "Acetylcholinesterase inhibitor", "Alzheimer's Dementia, Parkinson's Dementia", "Nausea, Vomiting, Diarrhea, Dizziness","Medication is not proven to be effective at this stage of illness. Reduce pill burden and side effects.", "Tapering not required", "Delirium, Hallucinations, Dry Mouth, Constipation, Blurred Vision");
-insert into medications (Generic_Name, Brand_Name, Class, Uses, Side_Effects, Rationale, DC_Plan, Withdrawal)
-values ("Metoprolol Tartrate", "Lopressor", "Beta-blocker", "Hypertension, Heart Failure, Angina, MI", "Hypotension, Bradycardia, Dizziness, Fatigue","Patients can remain normotensive after withdrawal.","Reduce dose by 50% weekly for two weeks.  If no withdrawal symptoms, stop medication", "Chest Pain, Increased Heart Rate, Increased Blood Pressure, Anxiety");
-insert into medications (Generic_Name, Brand_Name, Class, Uses, Side_Effects, Rationale, DC_Plan, Withdrawal)
-values ("Metoprolol Succinate", "Toprol XL", "Beta-blocker", "Hypertension, Heart Failure, Angina, MI", "Hypotension, Bradycardia, Dizziness, Fatigue","Patients can remain normotensive after withdrawal.", "Reduce dose by 50% weekly for two weeks.  If no withdrawal symptoms, stop medication", "Chest Pain, Increased Heart Rate, Increased Blood Pressure, Anxiety");
-insert into medications (Generic_Name, Brand_Name, Class, Uses, Side_Effects, Rationale, DC_Plan, Withdrawal)
-values ("Atenolol", "Tenormin", "Beta-blocker", "Hypertension, Angina, MI", "Hypotension, Bradycardia, Dizzines, Fatigue","Patients can remain normotensive after withdrawal.", "Reduce dose by 50% weekly for two weeks.  If no withdrawal symptoms, stop medication", "Chest Pain, Increased Heart Rate, Increased Blood Pressure, Anxiety");
-insert into medications (Generic_Name, Brand_Name, Class, Uses, Side_Effects, Rationale, DC_Plan, Withdrawal)
-values ("Memantine", "Namenda", "NMDA receptor antagonist", "Alzheimer's Dementia", "Constipation, Diarrhea, Headache","Medication is not proven to be effective at this stage of illness. Reduce pill burden and side effects.", "Tapering not required", "Insomnia, Delusions");
-insert into medications (Generic_Name, Brand_Name, Class, Uses, Side_Effects, Rationale, DC_Plan, Withdrawal)
-values ("Atorvastatin", "Lipitor", "Statin", "Dyslipidemia", "Diarrhea, Arthralgia, Nasopharyngitis","Longterm cardiac risk is no longer a concern.", "Tapering not required", "None");
-insert into medications (Generic_Name, Brand_Name, Class, Uses, Side_Effects, Rationale, DC_Plan, Withdrawal)
-values ("Simvastatin", "Zocor", "Statin", "Dyslipidemia", "Abdominal Pain, Constipation, Atrial Fibrillation, Myalgia","Longterm cardiac risk is no longer a concern.", "Tapering not required", "None");
-insert into medications (Generic_Name, Brand_Name, Class, Uses, Side_Effects, Rationale, DC_Plan, Withdrawal)
-values ("Lovastatin", "Mevacor", "Statin", "Dyslipidemia", "Flatulence, Constipation, Nausea, Myalgia","Longterm cardiac risk is no longer a concern.", "Tapering not required", "None");
-insert into medications (Generic_Name, Brand_Name, Class, Uses, Side_Effects, Rationale, DC_Plan, Withdrawal)
-values ("Rosuvastatin", "Crestor", "Statin", "Dyslipidemia", "Myalgia, Headache, Nausea, Constipation", "Longterm cardiac risk is no longer a concern.", "Tapering not required", "None");
-insert into medications (Generic_Name, Brand_Name, Class, Uses, Side_Effects, Rationale, DC_Plan, Withdrawal)
-values ("Alprazolam", "Xanax", "Benzodiazepine", "Anxiety, Panic Disorder", "Drowsiness, Fatigue, Sedation, Ataxia, Memory Impairment, Cognitive Dysfunction, Dysarthria, Dizziness, Weight Gain/Loss, Difficulty Urinating","If patient has paradoxical reaction, consider other classes of medication", "Reduce dose by 25% weekly", "Insomnia, Anxiety, Tremor, Hallucinations, Seizures, Delirium");
-insert into medications (Generic_Name, Brand_Name, Class, Uses, Side_Effects, Rationale, DC_Plan, Withdrawal)
-values ("Metformin", "Glucophage", "Biguanide", "Diabetes Type 2", "Nausea, Vomiting, Diarrhea, Dyspepsia","Focus becomes limiting hypo or hyperglycemic episodes rather than macro/microvascular damage. Medication can accumulate as patient declines leading to lactic acidosis.", "Tapering not required", "Increased thirst, Increased urination");
-insert into medications (Generic_Name, Brand_Name, Class, Uses, Side_Effects, Rationale, DC_Plan, Withdrawal)
-values ("Glyburide", "Diabeta", "Sulfonylurea", "Diabetes Type 2", "Hypoglycemia, Nausea, Heartburn","Focus becomes limiting hypo or hyperglycemic episodes rather than macro/microvascular damage.", "Tapering not required", "Increased thirst, Increased urination");
-insert into medications (Generic_Name, Brand_Name, Class, Uses, Side_Effects, Rationale, DC_Plan, Withdrawal)
-values ("Glipizide", "Glucotrol", "Sulfonylurea", "Diabetes Type 2", "Hypoglycemia","Focus becomes limiting hypo or hyperglycemic episodes rather than macro/microvascular damage.", "Tapering not required", "Increased thirst, Increased urination");
+insert into medications
+    (Generic_Name, Brand_Name, Class, Uses, Side_Effects, Rationale, DC_Plan, Withdrawal)
+values
+    ("Donepezil", "Aricept", "Acetylcholinesterase inhibitor", "Alzheimer's Dementia", "Nausea, Vomiting, Diarrhea, Insomnia", "Medication is not proven to be effective at this stage of illness. Reduce pill burden and side effects.", "Tapering not required", "Delirium, Hallucinations, Dry Mouth, Constipation, Blurred Vision");
+insert into medications
+    (Generic_Name, Brand_Name, Class, Uses, Side_Effects, Rationale, DC_Plan, Withdrawal)
+values
+    ("Rivastigmine", "Exelon", "Acetylcholinesterase inhibitor", "Alzheimer's Dementia, Parkinson's Dementia", "Nausea, Vomiting, Diarrhea, Dizziness", "Medication is not proven to be effective at this stage of illness. Reduce pill burden and side effects.", "Tapering not required", "Delirium, Hallucinations, Dry Mouth, Constipation, Blurred Vision");
+insert into medications
+    (Generic_Name, Brand_Name, Class, Uses, Side_Effects, Rationale, DC_Plan, Withdrawal)
+values
+    ("Metoprolol Tartrate", "Lopressor", "Beta-blocker", "Hypertension, Heart Failure, Angina, MI", "Hypotension, Bradycardia, Dizziness, Fatigue", "Patients can remain normotensive after withdrawal.", "Reduce dose by 50% weekly for two weeks.  If no withdrawal symptoms, stop medication", "Chest Pain, Increased Heart Rate, Increased Blood Pressure, Anxiety");
+insert into medications
+    (Generic_Name, Brand_Name, Class, Uses, Side_Effects, Rationale, DC_Plan, Withdrawal)
+values
+    ("Metoprolol Succinate", "Toprol XL", "Beta-blocker", "Hypertension, Heart Failure, Angina, MI", "Hypotension, Bradycardia, Dizziness, Fatigue", "Patients can remain normotensive after withdrawal.", "Reduce dose by 50% weekly for two weeks.  If no withdrawal symptoms, stop medication", "Chest Pain, Increased Heart Rate, Increased Blood Pressure, Anxiety");
+insert into medications
+    (Generic_Name, Brand_Name, Class, Uses, Side_Effects, Rationale, DC_Plan, Withdrawal)
+values
+    ("Atenolol", "Tenormin", "Beta-blocker", "Hypertension, Angina, MI", "Hypotension, Bradycardia, Dizzines, Fatigue", "Patients can remain normotensive after withdrawal.", "Reduce dose by 50% weekly for two weeks.  If no withdrawal symptoms, stop medication", "Chest Pain, Increased Heart Rate, Increased Blood Pressure, Anxiety");
+insert into medications
+    (Generic_Name, Brand_Name, Class, Uses, Side_Effects, Rationale, DC_Plan, Withdrawal)
+values
+    ("Memantine", "Namenda", "NMDA receptor antagonist", "Alzheimer's Dementia", "Constipation, Diarrhea, Headache", "Medication is not proven to be effective at this stage of illness. Reduce pill burden and side effects.", "Tapering not required", "Insomnia, Delusions");
+insert into medications
+    (Generic_Name, Brand_Name, Class, Uses, Side_Effects, Rationale, DC_Plan, Withdrawal)
+values
+    ("Atorvastatin", "Lipitor", "Statin", "Dyslipidemia", "Diarrhea, Arthralgia, Nasopharyngitis", "Longterm cardiac risk is no longer a concern.", "Tapering not required", "None");
+insert into medications
+    (Generic_Name, Brand_Name, Class, Uses, Side_Effects, Rationale, DC_Plan, Withdrawal)
+values
+    ("Simvastatin", "Zocor", "Statin", "Dyslipidemia", "Abdominal Pain, Constipation, Atrial Fibrillation, Myalgia", "Longterm cardiac risk is no longer a concern.", "Tapering not required", "None");
+insert into medications
+    (Generic_Name, Brand_Name, Class, Uses, Side_Effects, Rationale, DC_Plan, Withdrawal)
+values
+    ("Lovastatin", "Mevacor", "Statin", "Dyslipidemia", "Flatulence, Constipation, Nausea, Myalgia", "Longterm cardiac risk is no longer a concern.", "Tapering not required", "None");
+insert into medications
+    (Generic_Name, Brand_Name, Class, Uses, Side_Effects, Rationale, DC_Plan, Withdrawal)
+values
+    ("Rosuvastatin", "Crestor", "Statin", "Dyslipidemia", "Myalgia, Headache, Nausea, Constipation", "Longterm cardiac risk is no longer a concern.", "Tapering not required", "None");
+insert into medications
+    (Generic_Name, Brand_Name, Class, Uses, Side_Effects, Rationale, DC_Plan, Withdrawal)
+values
+    ("Alprazolam", "Xanax", "Benzodiazepine", "Anxiety, Panic Disorder", "Drowsiness, Fatigue, Sedation, Ataxia, Memory Impairment, Cognitive Dysfunction, Dysarthria, Dizziness, Weight Gain/Loss, Difficulty Urinating", "If patient has paradoxical reaction, consider other classes of medication", "Reduce dose by 25% weekly", "Insomnia, Anxiety, Tremor, Hallucinations, Seizures, Delirium");
+insert into medications
+    (Generic_Name, Brand_Name, Class, Uses, Side_Effects, Rationale, DC_Plan, Withdrawal)
+values
+    ("Metformin", "Glucophage", "Biguanide", "Diabetes Type 2", "Nausea, Vomiting, Diarrhea, Dyspepsia", "Focus becomes limiting hypo or hyperglycemic episodes rather than macro/microvascular damage. Medication can accumulate as patient declines leading to lactic acidosis.", "Tapering not required", "Increased thirst, Increased urination");
+insert into medications
+    (Generic_Name, Brand_Name, Class, Uses, Side_Effects, Rationale, DC_Plan, Withdrawal)
+values
+    ("Glyburide", "Diabeta", "Sulfonylurea", "Diabetes Type 2", "Hypoglycemia, Nausea, Heartburn", "Focus becomes limiting hypo or hyperglycemic episodes rather than macro/microvascular damage.", "Tapering not required", "Increased thirst, Increased urination");
+insert into medications
+    (Generic_Name, Brand_Name, Class, Uses, Side_Effects, Rationale, DC_Plan, Withdrawal)
+values
+    ("Glipizide", "Glucotrol", "Sulfonylurea", "Diabetes Type 2", "Hypoglycemia", "Focus becomes limiting hypo or hyperglycemic episodes rather than macro/microvascular damage.", "Tapering not required", "Increased thirst, Increased urination");
