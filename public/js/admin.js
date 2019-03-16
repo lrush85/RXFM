cce59d6f158a01068e954b77b2fea3a1e537091b
lrush85 <lance.rush1@gmail.com>
2019-03-16 10:38:03 -0500

    Made changes to admin.js file, admin-database.

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,16 +1,30 @@
 $(function () {
     $(".submit").on("click", function (event) {
         var id = $(this).data("id");
-        var dbAdmin = $(this).data("admin");
+        var addedMedication = $(this).data("addedMedication");
 
-        // Send the POST request.
+
+        $(".add-medication").on("submit", function(event) {
+            event.preventDefault();
+
+            var newMedication = {
+                genericName: $("#generic-name").val().trim(),
+                brandName: $("#brand-name").val().trim(),
+                class: $("#class").val().trim(),
+                uses: $("#uses").val().trim(),
+                sideEffects: $("#side-effects").val().trim(),
+                rational: $("#rational").val().trim(),
+                dcPlan: $("#dc-plan").val().trim(),
+                widthdrawl: $("#widthdrawl").val().trim()
+            };
+
+            // Send the POST request.
         $.ajax("/api/amin", {
             type: "POST",
-            data: data
-        }).then(
-            function () {
-                console.log("admin");
-                location.reload();
+            data: newMedication
+        }).then( function () {
+            console.log("Created a new medication");
+            location.reload();
             });
-    }
+        });
     });
