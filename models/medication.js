be79aa62fc982b5464cf5ee6b6c6997a746ab7fe
kupharmd05 <kupharmd05@gmail.com>
2019-03-16 13:36:32 -0500

    destroy and put routes working

--- models/medication.js
+++ models/medication.js
@@ -4,7 +4,7 @@ module.exports = function(sequelize, DataTypes) {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
-        len: [2]
+        len: [1]
       }
     },
     Brand_Name: {
