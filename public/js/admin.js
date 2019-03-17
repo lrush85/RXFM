638e68dc48a6b267d6eab4ca7097e736296412b7
Jaybird <jrmiller022@gmail.com>
2019-03-17 15:05:28 -0500

    updated admin.handlebars

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,16 +1,37 @@
-$(function () {
-    $(".submit").on("click", function (event) {
-        var id = $(this).data("id");
-        var dbAdmin = $(this).data("admin");
+$(document).ready(function () {
+    $("#add-medication").hide();
+    $("#delete-medication").hide();
+    $("#update-medication").hide();
+});
 
-        // Send the POST request.
-        $.ajax("/api/amin", {
-            type: "POST",
-            data: data
-        }).then(
-            function () {
-                console.log("admin");
-                location.reload();
-            });
-    }
+$("#add-button").on("click", function () {
+    $("#add-medication").show();
+});
+
+$("#add-submit").on("click", function (event) {
+    event.preventDefault();
+
+    $.post("/api/medication", function (data) {
+        var Medication = {
+            Generic_Name: $("#generic-name").val().trim(),
+            Brand_Name: $("#brand-name").val().trim(),
+            Class: $("#class").val().trim(),
+            Uses: $("#uses").val().trim(),
+            Side_Effects: $("#side-effects").val().trim(),
+            Rationale: $("#rationale").val().trim(),
+            DC_Plan: $("#dc-plan").val().trim(),
+            Withdrawal: $("#withdrawal").val().trim()
+        }
+
+        console.log(Medication);
     });
+});
+
+$("#delete-button").on("click", function () {
+    $("#delete-medication").show();
+});
+
+$("#update-button").on("click", function () {
+    $("#update-medication").show();
+});
+
