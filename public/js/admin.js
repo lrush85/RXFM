e3f0c5d902611e255ad1ed991d745709f71084ca
kupharmd05 <kupharmd05@gmail.com>
2019-03-17 15:44:37 -0500

    Jay sucks

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,30 +1,36 @@
-$(function () {
-    $(".submit").on("click", function (event) {
-        var id = $(this).data("id");
-        var addedMedication = $(this).data("addedMedication");
-
-
-        $(".add-medication").on("submit", function(event) {
-            event.preventDefault();
-
-            var newMedication = {
-                Generic_Name: $("#generic-name").val().trim(),
-                Brand_Name: $("#brand-name").val().trim(),
-                Class: $("#class").val().trim(),
-                Uses: $("#uses").val().trim(),
-                Side_Effects: $("#side-effects").val().trim(),
-                Rationale: $("#rational").val().trim(),
-                DC_Plan: $("#dc-plan").val().trim(),
-                Widthdrawl: $("#widthdrawl").val().trim(),
-            };
-
-            // Send the POST request.
-        $.ajax("/api/amin", {
-            type: "POST",
-            data: newMedication
-        }).then( function () {
-            console.log("Created a new medication");
-            location.reload();
-            });
-        });
+$(document).ready(function () {
+    $("#add-medication").hide();
+    $("#delete-medication").hide();
+    $("#update-medication").hide();
+ });
+ 
+ $("#add-button").on("click", function () {
+    $("#add-medication").show();
+ });
+ 
+ $("#add-submit").on("click", function (event) {
+    event.preventDefault();
+ 
+    $.post("/api/medication", function (data) {
+        var Medication = {
+            Generic_Name: $("#generic-name").val().trim(),
+            Brand_Name: $("#brand-name").val().trim(),
+            Class: $("#class").val().trim(),
+            Uses: $("#uses").val().trim(),
+            Side_Effects: $("#side-effects").val().trim(),
+            Rationale: $("#rationale").val().trim(),
+            DC_Plan: $("#dc-plan").val().trim(),
+            Withdrawal: $("#withdrawal").val().trim()
+        }
+ 
+        console.log(Medication);
     });
+ });
+ 
+ $("#delete-button").on("click", function () {
+    $("#delete-medication").show();
+ });
+ 
+ $("#update-button").on("click", function () {
+    $("#update-medication").show();
+ });
